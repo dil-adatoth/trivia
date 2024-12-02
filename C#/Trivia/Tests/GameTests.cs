aaea0bc118cb6863424733e9de3d9ecc637f9f02
Adam Toth <adatoth@diligent.com>
2024-12-02 11:45:32 +0100

    Set up Verify

---
+++ C#/Trivia/Tests/GameTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Trivia;
+using VerifyXunit;
+using Xunit;
+
+namespace Tests;
+public class GameTests
+{
+    public class HowManyPlayers
+    {
+        [Fact]
+        public Task Should_ReturnZero_When_NoPlayerIsAdded()
+        {
+            var sut = new Game();
+
+            var result = sut.HowManyPlayers();
+
+            return Verifier.Verify(result);
+        }
+    }
+}
